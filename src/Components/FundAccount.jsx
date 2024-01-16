aba4d39fcc04f7838506ecafec574fc2b37502e0
Yusuf Shamsudeen <yusufshamsudeen77@gmail.com>
2024-01-16 21:20:14 +0100

    adjust

--- src/Components/FundAccount.jsx
+++ src/Components/FundAccount.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function FundAccount() {
   return (
-    <div className='font-poppins w-full h-[90vh] flex flex-col justify-center items-center'>
-      <div className=' flex flex-col items-center gap-16'>
+    <div className='font-poppins w-full h-[100vh] flex flex-col justify-center items-center'>
+      <div className=' flex flex-col items-center gap-16 sm:gap-11'>
         <div>
             <h1 className=' text-[23px]'>Withdraw Your Funds</h1>
         </div>
         <form className=' flex flex-col gap-[40px] items-start'>
             <div className=' flex flex-col gap-3 '>
                     <label className='' htmlFor="amount">Bank</label>
-                <select name="" id="" className=' border-[1px] rounded-[10px] outline-none w-[30vw] h-[6vh] pl-5'>
+                <select name="" id="" className=' border-[1px] rounded-[10px] outline-none w-[30vw] h-[6vh] pl-5 sm:w-[80vw]'>
                     <option value="">..</option>
                     <option value="">UBA</option>
                     <option value="">Gt Bank</option>
@@ -19,7 +19,7 @@ function FundAccount() {
             <div className=' flex flex-col gap-3 items-end '>
                 <div className='flex flex-col gap-3 '>
                     <label htmlFor="amount">Account Number </label>
-                    <input className='border-[1px] rounded-[10px] outline-none w-[30vw] h-[6vh] pl-11 focus:border-[#F178B6]  duration-150' type="text" placeholder='1234567890' />
+                    <input className='border-[1px] rounded-[10px] outline-none w-[30vw] h-[6vh] pl-11 focus:border-[#F178B6] sm:w-[80vw]  duration-150' type="text" placeholder='1234567890' />
                 </div>
                 <div>
                     <p className=' text-[14px]'>You’re about to withdraw to: <span className=' text-[#7879F1]'>Ismaheel Abdulmaleeq</span></p>
@@ -27,7 +27,7 @@ function FundAccount() {
             </div>
                 <div className=' flex flex-col gap-3 '>
                     <label htmlFor="amount">Amount </label>
-                    <input className='border-[1px] rounded-[10px] outline-none w-[30vw] h-[6vh] pl-11 focus:border-[#F178B6]  duration-150' type="text" placeholder='Enter Amount' />
+                    <input className='border-[1px] rounded-[10px] outline-none w-[30vw] h-[6vh] pl-11 focus:border-[#F178B6] sm:w-[80vw]  duration-150' type="text" placeholder='Enter Amount' />
                 </div>
               
             <button className=' bg-[#7879F1] px-6  py-3 rounded-md text-[white]'>Withdraw</button>
